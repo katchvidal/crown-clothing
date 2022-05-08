141f49044969e2a80451c67caa41bcb7f3575ca1
Katch Vidal <katchvidal@gmail.com>
2022-05-08 18:23:26 -0500

    update: directory components template

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,18 @@
+
+import './directory.style.scss'
+import React from 'react'
+import CategoryItem from '../category-item/category-item.component'
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="categories-container">
+    {
+      categories.map(( category ) => (
+        <CategoryItem category={ category } key={ category.id } />
+      ))
+    }
+  </div>
+  )
+}
+
+export default Directory;
